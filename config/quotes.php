92a0333c90241aea720bba3ceb8c888321ae17b9
Dean Laverty <deannooel@gmail.com>
2024-07-03 17:10:29 +0100

    Allow env overrides

--- config/quotes.php
+++ config/quotes.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'driver' => 'kanye',
-    'api_url' => 'https://api.kanye.rest',
-    'cache_ttl' => 120,
+    'api_url' => env('QUOTES_API_URL', 'https://api.kanye.rest'),
+    'cache_ttl' => env('QUOTES_CACHE_TTL', 120),
 ];
